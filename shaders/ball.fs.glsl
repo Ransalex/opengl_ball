990e609c34fd88fb232ba564f499df4ad7625852
Ransalex <ransalex@gmail.com>
2014-08-31 21:51:16 +0400

    - добавил rim light

--- shaders/ball.fs.glsl
+++ shaders/ball.fs.glsl
@@ -23,6 +23,18 @@ out vec4 outColor;
 uniform vec3 specular_albedo = vec3(0.7);
 uniform float specular_power = 100.0;
 uniform vec3 ambient = vec3(0.1, 0.1, 0.1);
+//uniform vec3 rim_color = vec3(0.3, 0.3, 0.3);
+uniform float rim_power = 5.0;
+
+vec3 calculate_rim(vec3 N, vec3 V, vec3 rim_color)
+{
+    float f = 1.0 - dot(N, V);
+
+    f = smoothstep(0.0, 1.0, f);
+    f = pow(f, rim_power);
+
+    return f * rim_color;
+}
 
 void main(void)
 {
@@ -44,8 +56,12 @@ void main(void)
    vec3 V = normalize(fs_in.view_vector);
    vec3 R = reflect(-L, N);
 
-   vec3 diffuse = max(dot(N, L), 0.0) * vec3(mix(ball_color, cube_color, 0.3));
+   vec3 diffuse  = max(dot(N, L), 0.0) * vec3(mix(ball_color, cube_color, 0.3));
    vec3 specular = pow(max(dot(R, V), 0.0), specular_power) * specular_albedo;
 
-   outColor = vec4(ambient + diffuse + specular, 1.0)+ball_color*0.1;
+   vec3 amount_color = ambient + diffuse + specular+ball_color.xyz*0.1;
+
+   vec3 rim   = calculate_rim(N, V, amount_color);
+
+   outColor = vec4(amount_color + rim, 1.0);
 }
